ac689d3693a14e1b3065c3753ceb01a83c59ebc1
João Paulo Fricks <jpfricks@gmail.com>
2025-07-11 16:20:41 -0300

    feat: Add route for RecordRoomAudio component in the app

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CreateRoom } from "./pages/create-room";
 import { RoomDetails } from "./pages/room";
+import { RecordRoomAudio } from "./pages/record-room-audio";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ export function App() {
           <Route index element={<CreateRoom />} />
           <Route path="/room" element={<Navigate to="/" replace />} />
           <Route path="/room/:id" element={<RoomDetails />} />
+          <Route path="/room/:id/audio" element={<RecordRoomAudio />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
