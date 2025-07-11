71576fe8a7bd40b52bd54dca020c6a0c2c21dfae
João Paulo Fricks <jpfricks@gmail.com>
2025-07-10 23:02:26 -0300

    feat: Implement useCreateRoom hook for room creation with mutation handling

---
+++ src/hooks/use-create-room.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { CreateRoomRequest } from "@/http/types/create-room-request";
+import type { CreateRoomReponse } from "@/http/types/create-room-response";
+
+export function useCreateRoom() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: CreateRoomRequest) => {
+      const response = await fetch("http://localhost:3333/rooms", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create room");
+      }
+
+      const result: CreateRoomReponse = await response.json();
+
+      return result;
+    },
+    onSuccess: () => {
+      // Invalidate the rooms query to refresh the list of rooms
+      queryClient.invalidateQueries({ queryKey: ["get-rooms"] });
+      // Optionally, you can redirect the user to the newly created room
+      // window.location.href = `/room/${result.roomId}`;
+    },
+  });
+}
